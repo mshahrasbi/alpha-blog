c18a279cddb2f490d1e7836b49fdf211dac7c0c1
mshahrasbi <mshahrasbi@msn.com>
2019-08-02 13:25:41 -0700

    complete the authentication our app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,9 @@ class ArticlesController < ApplicationController
     # have set the @article
     before_action :set_article, only: [:edit, :update, :show, :destroy]
 
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
     def new 
         @article = Article.new
     end
@@ -59,5 +62,12 @@ class ArticlesController < ApplicationController
 
         def set_article
             @article = Article.find(params[:id])
+        end
+
+        def require_same_user
+            if current_user != @article.user
+                flash[:danger] = "You can only edit or delete your own articles"
+                redirect_to root_path
+            end
         end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
     # have set the @user
     before_action :set_user, only: [:edit, :update, :show]
 
+    before_action :require_same_user, only: [:edit, :update]
+
     def new
         @user = User.new
     end
@@ -42,11 +44,18 @@ class UsersController < ApplicationController
     end
 
     private
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+        def user_params
+            params.require(:user).permit(:username, :email, :password)
+        end
 
-    def set_user
-        @user = User.find(params[:id])
-    end
+        def set_user
+            @user = User.find(params[:id])
+        end
+
+        def require_same_user
+            if current_user != @user
+                flash[:danger] = "You can only edit your own articles"
+                redirect_to root_path
+            end
+        end        
 end

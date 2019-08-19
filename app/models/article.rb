4c4ab4cb5ad41b1c5b244a82604317b9b2250f41
mshahrasbi <mshahrasbi@msn.com>
2019-08-19 16:45:48 -0700

    create association for article and category - m to m

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@
 
 class Article < ActiveRecord::Base
     belongs_to :user
+    has_many :article_categories                        # has many categories
+    has_many :categories, through: :article_categories  # has many categories through article_categories
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 30, maximum: 300}
     validates :user_id, presence: true
